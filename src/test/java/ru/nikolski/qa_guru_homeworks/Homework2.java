21a96b4c89c1799335aed17dc12ff0d25ce07400
Anton248 <31551544+Anton248@users.noreply.github.com>
2023-03-30 01:28:09 +0300

    added class some tests

--- src/test/java/ru/nikolski/qa_guru_homeworks/Homework2.java
+++ src/test/java/ru/nikolski/qa_guru_homeworks/Homework2.java
@@ -1,15 +1,64 @@
 package ru.nikolski.qa_guru_homeworks;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
+//testing all fields, checkboxes and buttons at https://demoqa.com/automation-practice-form
 public class Homework2 extends TestBase{
+    String zero = ""; //utility string
 
-//testing all forms at https://demoqa.com/automation-practice-form
     @Test
     void homework2() {
+
+        //values for input fields, checkboxes, etc
+        String firstName = "Ivan",
+                lastName = "Ivanov",
+                email = "ivanov@ivanov.com",
+                gender = "Male",
+                mobileNumber = "1234567890",
+                dayOfBirth = "2",
+                monthOfBirth = "February",
+                yearOfBirth = "2000",
+                subject1 = "Chemistry",
+                subject2 = "Computer Science",
+                hobbies1 = "sports",
+                hobbies2 = "reading",
+                hobbies3 = "music",
+                file = "1.jpg",
+                currentAddress = "Москва, ул. Уличная, д.5, кв.111",
+                state = "Uttar Pradesh",
+                city = "Agra";
+
+        //filling the input fields, clicking the checkboxes, etc
         open("/automation-practice-form");
+
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        //$("#genterWrapper").selectRadio(gender); //doesn't work
+        //$("#gender-radio-1").parent().click(); //bad implementation
+        $("#genterWrapper").$(By.cssSelector("input[value=\"" + gender + "\"]")).parent().click();
+        $("#userNumber").setValue(mobileNumber);
+
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").selectOption(yearOfBirth);
+        $(".react-datepicker__month-select").selectOption(monthOfBirth);
+        if (dayOfBirth.length() == 1) zero = "0";
+        $(".react-datepicker__day--0" + zero + dayOfBirth).click();
+
+        $("#subjectsInput").setValue(subject1).pressEnter();
+        $("#subjectsInput").setValue(subject2).pressEnter();
+
+
+
+
+
+
+
 
     }
 }
--- src/test/java/ru/nikolski/qa_guru_homeworks/TestBase.java
+++ src/test/java/ru/nikolski/qa_guru_homeworks/TestBase.java
@@ -3,11 +3,16 @@ package ru.nikolski.qa_guru_homeworks;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 // Preparing browser for all tests
 public class TestBase {
+    String pathToPictures = "pictures/1.jpg"; //path to pictures which used in tests
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";  //site to test
-        //Configuration.holdBrowserOpen = true; // testing the tests themselves
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = true; // show browser to test the tests themselves
     }
 }
